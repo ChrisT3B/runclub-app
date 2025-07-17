d8665b413e2fba5bd7591f010d896f72fbc26dfa
Chris Tompkins <christompkins@3bprojects.co.uk>
2025-07-17 05:55:52 +0100

    updated auth path

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './modules/auth/context/AuthContext';
 import { AppContent } from './AppContent';
+import { AuthContent } from './modules/auth/components/AuthContent';
 import './styles/fonts.css';
-import './index.css'; // Make sure this imports your main CSS file
+import './index.css';
 
 function App() {
   return (
     <AuthProvider>
-      <AppContent />
+      <Router>
+        <Routes>
+          {/* Handle /auth path specifically for email verification */}
+          <Route path="/auth" element={<AuthContent />} />
+          
+          {/* All other paths go to AppContent (which handles auth state) */}
+          <Route path="/*" element={<AppContent />} />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
