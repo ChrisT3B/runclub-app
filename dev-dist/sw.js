4ccb071b7bafa321bdf1b82556654fe9b8966993
Chris Tompkins <christompkins@3bprojects.co.uk>
2025-08-16 07:12:27 +0100

    Day 1: CSS foundation complete - some styling needs extraction

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ht92dh26dc"
+    "revision": "0.2lco0pg71"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
